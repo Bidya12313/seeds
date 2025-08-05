6c0a2118758fbba78508b9ab5c9b6f0b2cf83eb9
Bidya12313 <bogdan.matsenko@gmail.com>
2025-08-05 22:05:01 +0300

    created new css styles

--- flower/static/scripts/products.js
+++ flower/static/scripts/products.js
@@ -70,12 +70,12 @@ let category = params.get("category");
 cat_arr.forEach((item) => {
   if (item.name === category) {
     document.getElementById("category-list").innerHTML += `
-                            <a href="prducts?category=${item.slug}"><li id="${item.name}" class="categories chosen">
+                            <a href="/products?category=${item.slug}"><li id="${item.name}" class="categories chosen">
                             ${item.name} →
                         </li></a>`;
   } else {
     document.getElementById("category-list").innerHTML += `
-                            <a href="products?category=${item.slug}"><li id="${item.name}" class="categories">
+                            <a href="/products?category=${item.slug}"><li id="${item.name}" class="categories">
                             ${item.name} →
                         </li></a>`;
   }
